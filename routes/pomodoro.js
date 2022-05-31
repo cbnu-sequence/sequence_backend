15ece4ea75d31fb4366489cd26faa7faf7268d25
wnstjd4778 <smy4778@naver.com>
2022-05-31 21:04:17 +0900

    Add getPomodoro functions

--- routes/pomodoro.js
+++ routes/pomodoro.js
@@ -8,4 +8,7 @@ router.post("/:pomodoroId", verifiedUser, controller.finishedPomodoro);
 router.get("/ranking/day", controller.getPomodorosRankingByDay);
 router.get("/ranking/week", controller.getPomodorosRankingByWeek);
 router.get("/ranking/month", controller.getPomodorosRankingByMonth);
+router.get("/user/me", verifiedUser, controller.getPomodorosByUser);
+router.get("/:pomodoroId", verifiedUser, controller.getPomodoro);
+router.get("/", verifiedUser, controller.getPomodorosByUser);
 module.exports = router;
